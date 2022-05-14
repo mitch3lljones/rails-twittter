c79f1dbdf56b4518a27a168e64913dcb6245b4c1
Mitchell Jones <mitchell.jones@eagles.oc.edu>
2022-05-13 19:26:13 -0500

    update rails to 7.0.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_155304) do
+ActiveRecord::Schema[6.1].define(version: 2020_07_01_155304) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
